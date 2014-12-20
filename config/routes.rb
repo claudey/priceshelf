5cf32c48d1e5d1a837d11f9a4a378fe0522b1597
Claude Ayitey <clauditey@gmail.com>
2014-12-20 02:45:13 +0000

    changed minor text on home page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root to: 'pages#index'
-  # get root => 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
